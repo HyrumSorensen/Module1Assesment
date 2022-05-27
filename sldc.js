6e807f0bc055f6cf44b2601e4f842bd6e9a406d2
Hyrum Sorensen <hyrumgary@gmail.com>
2022-05-27 10:04:49 -0600

    Initial commit

---
+++ sldc.js
@@ -0,0 +1,25 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+*/
+
+let Planning = 'Determining a need for a specific program, who will be the end users, what the development will cost, and how long it will take.'
+let AnalysisOfRequirements = 'The process of defining user expectations for a new software being built or modified.'
+let Design = 'The transforming of the requirements into complete and detailed system design specifications.'
+let Implementation = 'user notification, user training, installation of hardware, installation of software onto production computers, and integration of the system into daily work processes.'
+let TestingAndIntegration = 'The Testing of all software and hardware configuration item components until fully functional.'
+let Maintain = 'software upgrades, repairs, and fixes of the software if it breaks.'
